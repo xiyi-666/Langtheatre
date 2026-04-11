5b11a3128557d90d4ac900b3a9ccf0a6a439755e
unknown <xiyi666@github.com>
2026-04-11 18:18:33 +0800

    fix(tauri): add lib target for android build

--- apps/client/src-tauri/src/main.rs
+++ apps/client/src-tauri/src/main.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("failed to run linguaquest tauri app");
+    linguaquest_client_lib::run();
 }
